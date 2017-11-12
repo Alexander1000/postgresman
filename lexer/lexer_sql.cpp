e76ff71c63535e61c06dbf57d19551125aca1c98
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2017-11-12 19:58:16 +0300

    array of keywords

--- lexer/lexer_sql.cpp
+++ lexer/lexer_sql.cpp
@@ -3,6 +3,14 @@ class LexerSQL : public Lexer
 public:
     LexerSQL(IOBuffer *buffer) : Lexer(buffer)
     {
-        this->keywords.addValue((char*) "create");
+        const char* keywords[] = {
+                "create",
+                "or",
+                "replace"
+        };
+
+        for (int i = 0; i < 3; ++i) {
+            this->keywords.addValue((char*) keywords[i]);
+        }
     }
 };
