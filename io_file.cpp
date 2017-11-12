9694e23b00ed69be58567f243e7409f80d06bcb7
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2017-11-12 13:47:44 +0300

    add io file

--- io_file.cpp
+++ io_file.cpp
@@ -1,4 +1,12 @@
-class IOFile
+#include "io_buffer.cpp"
+
+class IOFile : public IOBuffer
 {
-    
+public:
+    IOFile(char* fileName) : IOBuffer()
+    {
+        this->fileName = fileName;
+    }
+private:
+    char* fileName;
 };
