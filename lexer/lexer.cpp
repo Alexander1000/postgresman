4e8bd4294272122f7f953953fab9bb712d2e76f1
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2017-11-28 08:22:32 +0300

    changes

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,13 +1,3 @@
-#define LEXER_MODE_DEFAULT 0
-#define LEXER_MODE_ONELINE_COMMENT 1
-#define LEXER_MODE_MULTILINE_COMMENT 2
-#define LEXER_MODE_LEXEME_QUOTES 3
-#define LEXER_MODE_LEXEME_WORD 4
-#define LEXER_MODE_NUMERIC 5
-// $$ or $BODY$ quoted expressions
-#define LEXER_MODE_CUSTOM_QUOTE 6
-#define LEXER_MODE_CONTENT_QUOTED 7
-
 class Lexer
 {
 public:
